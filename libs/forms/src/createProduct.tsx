5d4bdceba1be8c56457b0584ac83c850fe47a453
Karthick Ragavendran <karthickragavendran@Karthicks-Mac-Studio.local>
2023-06-21 13:04:20 +0530

    create forms library

---
+++ libs/forms/src/createProduct.tsx
@@ -0,0 +1,25 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ReactNode } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+export const schemaCreateProduct = z.object({
+  name: z.string(),
+  plasticWeight: z.number(),
+})
+
+export type FormTypeCreateProduct = z.infer<typeof schemaCreateProduct>
+
+export const useFormCreateProduct = () =>
+  useForm<FormTypeCreateProduct>({
+    resolver: zodResolver(schemaCreateProduct),
+  })
+
+export const FormProviderCreateProduct = ({
+  children,
+}: {
+  children: ReactNode
+}) => {
+  const methods = useFormCreateProduct()
+  return <FormProvider {...methods}>{children}</FormProvider>
+}
